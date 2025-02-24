305b81a8cdce56d08b2fba8373e832c2faec10d4
majunseob <majunseob@gmail.com>
2025-02-24 17:57:52 +0900

    Hello World!

---
+++ CppStudy.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!\n";
+	std::cout << "Hello" << " World" << std::endl;
+	int numb = 123;
+	std::cout << numb << ' ' << 'A',
+		std::cout << ' ' << 3.14 << std::endl;
+
+	return 0;
+}
