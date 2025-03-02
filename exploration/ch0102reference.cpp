cfe863685ef5b03aebd8b8b6680950fe6dbcdcdc
majunseob <majunseob@gmail.com>
2025-03-02 22:32:38 +0900

    reference 수정

--- exploration/ch0102reference.cpp
+++ exploration/ch0102reference.cpp
@@ -2,15 +2,28 @@
 
 using namespace std;
 
+void modify(int& x)
+{
+	x = 100; // 원본 변수 변경됨
+}
+
 int main()
 {
-	int a = 10;
-	int& ref = a;
+	{
+		int a = 10;
+		int& ref = a;
 
-	cout << "a: " << a << " ref: " << ref << endl;
+		cout << "a: " << a << " ref: " << ref << endl;
 
-	ref = 20;
-	cout << "a: " << a << " ref: " << ref << endl;
+		ref = 20;
+		cout << "a: " << a << " ref: " << ref << endl;
+	}
+
+	{
+		int num = 5;
+		modify(num);
+		cout << num << endl;
+	}
 
 	return 0;
 }
